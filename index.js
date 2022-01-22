8bf082351bdb2e26d85ae09e8918f42dbe01eca1
BuildTools <matthewl64@hotmail.co.uk>
2022-01-22 21:40:22 +0000

    Added more intents for fuller coverage of functionality

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const log = require("coloured-logger")({ logName: "Application", useFiles: false });
 const Discord = require("discord.js");
-const client = new Discord.Client({ intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MEMBERS] });
+const client = new Discord.Client({
+    intents: [
+        Discord.Intents.FLAGS.GUILDS,
+        Discord.Intents.FLAGS.GUILD_MEMBERS,
+        Discord.Intents.FLAGS.GUILD_MESSAGES,
+        Discord.Intents.FLAGS.GUILD_INVITES,
+        Discord.Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+        Discord.Intents.FLAGS.DIRECT_MESSAGES
+    ]
+});
 const path = require("path");
 const fs = require("fs");
 
